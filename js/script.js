6ff35bee5d5f244a3426d79fd41a634752e778cd
Tetsuro Ishida <tetsuroishida1112@gmail.com>
2023-06-29 14:06:52 +0900

    initial commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+$(function(){
+    var $win = $(window),
+        $fv = $('.fv'),
+        $header = $('.header'),
+        fvHeight = $fv.outerHeight();
+        fixedClass = 'fixed';
+
+    $win.on('load scroll', function() {
+        var value = $(this).scrollTop();
+        if($win.width() > 768) {
+            if(value > fvHeight) {
+                $header.addClass(fixedClass);
+            } else {
+                $header.removeClass(fixedClass);
+            }
+        }
+    });
+});
+
+// スライダー
+$('.slider').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    fade: true,
+    speed: 1000,
+    cssEase: 'linear'
+});
+
+//ハンバーガーボタン
+$('.hamburger-btn').on('click', function() {
+    $('.header-nav').fadeToggle(300);
+    $(this).toggleClass('cross');
+    $('body').toggleClass('noscroll');
+});
+
+
+$(document).ready(function(){
+    if ($(window).width() < 768) {
+        $('.header-item').on('click', function() {
+            $('.header-nav').fadeToggle(300);
+            $('.hamburger-btn').toggleClass('cross');
+            $('body').toggleClass('noscroll');
+        })
+    }
+});
+
+
